c8a8c549e3d83ae862a9a6f075122919ca325ac2
pavelstarshinov@gmail.com <pav$sta2>
2022-01-20 23:56:29 +0300

    20/01 homework

--- Инкапсуляция, наследование, полиморфизм/Домашнее задание/task1_Date_property/main.py
+++ Инкапсуляция, наследование, полиморфизм/Домашнее задание/task1_Date_property/main.py
@@ -6,31 +6,84 @@ class Date:
     )
 
     def __init__(self, day: int, month: int, year: int):
-        self.day = day
-        self.month = month
-        self.year = year
+        self._day = day
+        self._month = month
+        self._year = year
 
-        self.is_valid_date(self.day, self.month, self.year)
+        self.is_valid_date(self._day, self._month, self._year)
 
-    # TODO какой декоратор следует применить?
-    def is_leap_year(self, year: int) -> bool:
+    @staticmethod
+    def is_leap_year(year: int) -> bool:
         """Проверяет, является ли год високосным"""
-        ...  # TODO записать условие проверки високосного года
+        if year % 4 == 0:
+            if not year % 100 == 0:
+                result = True
+            else:
+                if year // 100 % 4 == 0:
+                    result = True
+                else:
+                    result = False
+        else:
+            result = False
+        return result
 
-    def get_max_day(self, month: int, year: int) -> int:
+    @classmethod
+    def get_max_day(cls, month: int, year: int):
         """Возвращает максимальное количество дней в месяце для указанного года"""
-        ...  # TODO вернуть количество дней указанного месяца
+        max_days = cls.DAY_OF_MONTH[cls.is_leap_year(year)][month - 1]
+        return max_days
 
-    def is_valid_date(self, day: int, month: int, year: int) -> None:
+    @classmethod
+    def is_valid_date(cls, day: int, month: int, year: int):
         """Проверяет, является ли дата корректной"""
-        ...  # TODO если указанный набор день, месяц и год неверны, то вызвать ошибку ValueError
+        if not isinstance(day, int) or not isinstance(month, int) or not isinstance(year, int):
+            raise TypeError
+        if day <= 0 or month <= 0 or year <= 0:
+            raise ValueError("Не может быть нулем и отрицат")
+        elif month > 12:
+            raise ValueError("Месяцев в году 12")
+        elif day > cls.get_max_day(month, year):
+            raise ValueError("В этом месяце нет столько дней по календярю")
 
-    # TODO записать getter и setter для дня
+    @property
+    def day(self):
+        return self._day
 
-    # TODO записать getter и setter для месяца
+    @day.setter
+    def day(self, day):
+        if not isinstance(day, int):
+            raise TypeError("День должен быть целочисленным")
+        if not 0 < day <= 31:
+            raise ValueError("День д.б. больше 0 и не больше 31")
+        self._day = day
 
-    # TODO записать getter и setter для года
+    @property
+    def month(self):
+        return self._month
+
+    @month.setter
+    def month(self, month):
+        if not isinstance(month, int):
+            raise TypeError("Мес должен быть целочисленным")
+        if not 1 <= month <= 12:
+            raise ValueError("Мес д.б. больше 1 и не больше 12")
+        self._month = month
+
+    @property
+    def year(self):
+        return self._year
+
+    @year.setter
+    def year(self, year):
+        if not isinstance(year, int):
+            raise TypeError("Год должен быть целочисленным")
+        if not 0 < year:
+            raise ValueError("Год д.б. больше 0")
+        self._year = year
 
 
 if __name__ == "__main__":
-    ...
+    a = Date(12,12,1981)
+    print(a)
+    a.month = 13
+
--- Инкапсуляция, наследование, полиморфизм/Домашнее задание/task1_all_attr_all_method/main.py
+++ Инкапсуляция, наследование, полиморфизм/Домашнее задание/task1_all_attr_all_method/main.py
@@ -1,3 +1,27 @@
+
+class Parent:
+    atr_cls1 = 1
+    _atr_cls2 = 2
+    __atr_cls3 = 3
+
+    def __init__(self, atr1, atr2, atr3):
+        self.atr1 = atr1
+        self.__atr3 = atr3
+        self._atr2 = atr2
+
+    @classmethod
+    def meth(cls):
+        print(cls.atr_cls1)
+
+    @classmethod
+    def _meth2(cls):
+        print(cls.atr_cls1)
+
+    @classmethod
+    def __meth3(cls):
+        print(cls.atr_cls1)
+
+
 if __name__ == "__main__":
     # Write your solution here
     pass
